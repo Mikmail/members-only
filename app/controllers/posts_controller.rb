1386518ff1b4a917cfb22930c5afee7251cca8f6
Mikmail <mikael@rund.no>
2022-10-05 14:54:53 +0200

    authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user, only: [:new, :create, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -30,8 +30,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def authenticate_user!
-    #http_basic_authenticate_or_request_with name: "dhh", password: "secret"
+  def authenticate_user
+    authenticate_or_request_with_http_basic do |username, password| 
+      User.find_by_username(username).password == User.password
+    end
   end
 
   def post_params 
